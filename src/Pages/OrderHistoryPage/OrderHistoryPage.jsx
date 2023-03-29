85b407686a3d002a46fbf330000f900743b06070
apot <apotterton80@gmail.com>
2023-03-29 13:07:39 -0700

    pre-deploy

--- src/Pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/Pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -3,7 +3,6 @@ import { checkToken } from '../../utilities/users-service'
 export default function OrderHistoryPage() {
   async function handleCheckToken() {
     const expDate = await checkToken()
-    console.log(expDate)
   }
   
   return (
